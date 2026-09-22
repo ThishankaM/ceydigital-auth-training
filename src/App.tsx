c71700502f6fb0ecb63b0d3beff6928ab61a652f
ThishakaM <thishanka.methshan@gmail.com>
2026-09-22 12:32:24 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button type="primary">Button</Button>
+      <h1>Hello, Vite!</h1>
+    </>
+  )
+}
+
+export default App
